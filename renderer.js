af1b1f432b032c97b1eb2cc9d7926e5375f990ce
Farzain Majeed <contactclothinghousenow@gmail.com>
2018-05-02 20:04:36 -0400

    added ability to detect POTF screen via openCopenCV.js

--- renderer.js
+++ renderer.js
@@ -42,12 +42,14 @@ let curr = null;
 let heroClassPredictions = [];
 let heroProbabilityValues =[]
 
+let potgScreenIsOn = false;
+
 // Load model from AWS, and than start taking screenshots.
 const loadModel = async () => {
     console.log("Loading Model...");
     model =  await tf.loadModel('https://s3-us-west-2.amazonaws.com/mood1995/neural_net_all_heroes/model.json');
     console.log("Model loaded!");
-    setInterval(startScreen, 1000)
+    setInterval(startScreen, 250)
 }
 
 loadModel();
@@ -66,13 +68,17 @@ async function predict(imgElement) {
     let prob = classes[0].probability;
     let pred = classes[0].className;
 
+    console.log(pred);
+    console.log(prob);
+
+
     if(prob < 0.97) {
         console.log("Not a high enough probability");
         return;
     }
 
     // check last 5 predictions, and only then switch playlists.
-    if (heroClassPredictions.length == 3) {
+    if (heroClassPredictions.length == 4) {
         // remove oldest prediction to keep array at constant size < 5.
         heroClassPredictions.shift();
         heroProbabilityValues.shift()
@@ -95,7 +101,7 @@ async function predict(imgElement) {
         return
     }
 
-    if (newH != curr || curr == null) {
+    if (curr == null || newH != curr) {
         webview.removeEventListener('did-start-loading', loadstart)
         webview.removeEventListener('did-stop-loading', loadstop)
         webview.addEventListener('did-start-loading', loadstart)
@@ -109,11 +115,28 @@ async function predict(imgElement) {
 
 function startScreen() {
     screenshot().then((fullImage) => {
+        // path + "/screenshot.png"
+        // /Users/flynn/Downloads/Two/Screenshot (5)/Screenshot (22).png
         fs.writeFile(path + "/screenshot.png", fullImage, function(err) {
             if(err) {
                 return console.log(err);
             }
-            Jimp.read(fullImage, function (err, jimpImage) {
+            // Once every second, check if we are on the POTG screen.
+            Jimp.read("my_screen.jpg", function (err, jimpImage) {
+                if (err) throw err;
+                jimpImage.crop(35, 35, 210, 35)
+                     .quality(100)
+                     .write(path + "/screenshot-potg-cropped.jpg", function(err) {
+                         if (err) throw err;
+                         var image = new Image();
+                         image.onload = function () {
+                             checkPlayOfTheGame(image);
+                         };
+                         image.src = path + '/screenshot-potg-cropped.jpg?' + new Date().getTime();
+                     });
+            });
+
+            Jimp.read("my_screen.jpg", function (err, jimpImage) {
                 if (err) throw err;
                 jimpImage.crop(1650, 920, 170, 80)
                      .resize(85, 40)
@@ -127,6 +150,7 @@ function startScreen() {
                          image.src = path + '/screenshot-cropped.jpg?' + new Date().getTime();
                      });
             });
+
         });
     }).catch((err) => {
           console.log("Screenshot failed", err);
@@ -142,7 +166,7 @@ function checkChamp(arr, arrTwo) {
         var it = s.values();
         var first = it.next();
         // Lastly, we do a quick check to make sure we had a "100%" prediction at least once.
-        if (arrTwo.includes(1)) {
+        if (arrTwo.filter(item => item == 1).length >= 2) {
             return first.value;
         } else {
             return null;
@@ -177,4 +201,50 @@ async function getTopKClasses(logits, topK) {
     })
   }
   return topClassesAndProbs;
+}
+
+// function loadTemplate() {
+//     var image = new Image();
+//     image.onload = function () {
+//         template = image;
+//         template = cv.imread(template);
+//
+//     };
+//     image.src = path + '/template.jpg?' + new Date().getTime();
+// }
+//
+// loadTemplate();
+
+function checkPlayOfTheGame(imgElement) {
+    if (!(openCVReady)) {
+        console.log("OpenCV still not ready!");
+        return false;
+    }
+
+    if(template == null) {
+        console.log("Template still null");
+        return false;
+    }
+
+    let src = cv.imread(imgElement);
+    let dst = new cv.Mat();
+    let mask = new cv.Mat();
+    cv.matchTemplate(src, template, dst, cv.TM_CCOEFF_NORMED, mask);
+
+    // sq_diff normed 1 - minVal
+    // coeff_normed max_val
+
+    let result = cv.minMaxLoc(dst, mask);
+    console.log("template match gave us probability ... ", result.maxVal);
+    if(result.maxVal < 0.90) {
+        console.log("Didn't detect play of the game screen!");
+    }
+
+    // let maxPoint = result.maxLoc;
+    // let color = new cv.Scalar(255, 0, 0, 255);
+    // let point = new cv.Point(maxPoint.x + templ.cols, maxPoint.y + templ.rows);
+    // cv.rectangle(src, maxPoint, point, color, 2, cv.LINE_8, 0);
+    // cv.imshow('canvasOutput', src);
+    src.delete(); dst.delete(); mask.delete();
+    console.log("Loaded image as mat!");
 }
